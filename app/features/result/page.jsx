7e196274a28cee180eb92b95794dcde348a1cfdd
ayobami7 <adeyemoayobami300@yahoo.com>
2024-05-22 19:57:26 +0100

    results page

--- app/features/result/page.jsx
+++ app/features/result/page.jsx
@@ -1,29 +1,58 @@
+"use client"
+
 import React from 'react'
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination} from "@nextui-org/react";
+import { columns, data } from '@/constants/resultData';
+import { GrDownload } from 'react-icons/gr';
 
 const Result = () => {
   return (
-    <div>
-        <h1>Articulate struxture of C++ and Java</h1>
-        <p>toggle</p>
+    <div className='m-3'>
+
+        <div className='flex flex-row justify-between p-5'>
+            <h1 className='font-bold text-xl'>Articulate structure of C++ and Java</h1>
+            <button className='border-1 bg-blue-600 text-white p-2 rounded-md'>Publish Result</button>
+        </div>
+
+        <div className='flex flex-row max-w-fit m-5 rounded-md text-sm text-gray-400 bg-gray-200'>
+            <button className='px-3 py-2 w-32 '>Details</button>
+            <button className='rounded-md px-3 py-2 w-32 bg-blue-600'>Result</button>
+        </div>
 
         <Table 
             aria-label="table with dynamic content" 
-            selectionMode="multiple" 
             bottomContent={<div className="flex justify-center" >
                 <Pagination showControls color="default"/>
                 </div>}>
             <TableHeader columns={columns}>
                 {columns.map((column) => (
-                    <TableColumn key={column.key}>{column.label}</TableColumn>
+                    <TableColumn key={column.uid}>{column.name}</TableColumn>
                 ))}
             </TableHeader>
             <TableBody>
-                {rows.map((item) => (
-                    <TableRow key={item.invoice}>
-                        {columns.map((column) => (
-                            <TableCell key={column.key}>{item[column.key]}</TableCell>
-                        ))}
+                {data.map((item, index) => (
+                    <TableRow key={item.student_id}>
+                       
+                            <TableCell key={item.student_id}>{item.student_id}</TableCell>
+                            <TableCell key={item.student_id}>{item.name}</TableCell>
+                            <TableCell key={item.student_id}>
+                            <div className="border-1 text-sm flex rounded-md p-1 max-w-fit cursor-pointer">
+                                Download <GrDownload className='mx-3' />
+                            </div>
+                            </TableCell>
+                            <TableCell key={item.student_id}>
+                                <div className='flex flex-row'>
+                                    <input type="text" className='border-1 w-12' />
+                                    <p className=''>/50</p>
+                                </div>
+                            </TableCell>
+                            <TableCell key={item.student_id}>
+                                <div className='space-x-10 justify-center'>
+                                    <button className='border-1 text-blue-800 bg-gray-300 rounded-md py-1 px-2'>View file</button>
+                                    <button className='border-1 text-blue-400 bg-slate-200 rounded-md py-1 px-2'>Save</button>
+                                </div>
+                            </TableCell>
+                       
                     </TableRow>
                 ))}
             </TableBody>
--- constants/resultData.js
+++ constants/resultData.js
@@ -1,25 +1,32 @@
-export const data= [
-    [
-        {"name": "Alice Johnson", "student_id": "TIPSHGNM 2202001"},
-        {"name": "Bob Smith", "student_id": "TIPSHGNM 2202002"},
-        {"name": "Charlie Brown", "student_id": "TIPSHGNM 2202003"},
-        {"name": "David Lee", "student_id": "TIPSHGNM 2202004"},
-        {"name": "Eva Martinez", "student_id": "TIPSHGNM 2202005"},
-        {"name": "Franklin Davis", "student_id": "TIPSHGNM 2202006"},
-        {"name": "Grace Clark", "student_id": "TIPSHGNM 2202007"},
-        {"name": "Helen Garcia", "student_id": "TIPSHGNM 2202008"},
-        {"name": "Isaac Wright", "student_id": "TIPSHGNM 2202009"},
-        {"name": "Jack Robinson", "student_id": "TIPSHGNM 2202010"},
-        {"name": "Katherine Moore", "student_id": "TIPSHGNM 2202011"},
-        {"name": "Liam Hall", "student_id": "TIPSHGNM 2202012"},
-        {"name": "Mia Young", "student_id": "TIPSHGNM 2202013"},
-        {"name": "Nora King", "student_id": "TIPSHGNM 2202014"},
-        {"name": "Oliver Evans", "student_id": "TIPSHGNM 2202015"},
-        {"name": "Penelope Carter", "student_id": "TIPSHGNM 2202016"},
-        {"name": "Quinn Hill", "student_id": "TIPSHGNM 2202017"},
-        {"name": "Ryan Phillips", "student_id": "TIPSHGNM 2202018"},
-        {"name": "Sofia Brooks", "student_id": "TIPSHGNM 2202019"},
-        {"name": "Theodore Mitchell", "student_id": "TIPSHGNM 2202020"}
-    ]
-    
+export const columns = [
+    {name: "Student ID", uid: "Student ID", sortable: true},
+    {name: "Name", uid: "Name", sortable: true},
+    {name: "Attachments", uid: "Attachments"},
+    {name: "Marks", uid: "Marks"},
+    {name: "Actions", uid: "Actions"},
+  ];
+
+
+export const data=
+[
+    {name: "Alice Johnson", student_id: "TIPSHGNM 2202001"},
+    {name: "Bob Smith", student_id: "TIPSHGNM 2202002"},
+    {name: "Charlie Brown", student_id: "TIPSHGNM 2202003"},
+    {name: "David Lee", student_id: "TIPSHGNM 2202004"},
+    {name: "Eva Martinez", student_id: "TIPSHGNM 2202005"},
+    {name: "Franklin Davis", student_id: "TIPSHGNM 2202006"},
+    {name: "Grace Clark", student_id: "TIPSHGNM 2202007"},
+    {name: "Helen Garcia", student_id: "TIPSHGNM 2202008"},
+    {name: "Isaac Wright", student_id: "TIPSHGNM 2202009"},
+    {name: "Jack Robinson", student_id: "TIPSHGNM 2202010"},
+    {name: "Katherine Moore", student_id: "TIPSHGNM 2202011"},
+    {name: "Liam Hall", student_id: "TIPSHGNM 2202012"},
+    {name: "Mia Young", student_id: "TIPSHGNM 2202013"},
+    {name: "Nora King", student_id: "TIPSHGNM 2202014"},
+    {name: "Oliver Evans", student_id: "TIPSHGNM 2202015"},
+    {name: "Penelope Carter", student_id: "TIPSHGNM 2202016"},
+    {name: "Quinn Hill", student_id: "TIPSHGNM 2202017"},
+    {name: "Ryan Phillips", student_id: "TIPSHGNM 2202018"},
+    {name: "Sofia Brooks", student_id: "TIPSHGNM 2202019"},
+    {name: "Theodore Mitchell", student_id: "TIPSHGNM 2202020"}
 ]
